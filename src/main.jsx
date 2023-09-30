00494bd9fa6795b331e844978273f5532d9852e2
fathan nasrullah <fathannasrullah0@gmail.com>
2023-09-30 12:11:55 +0700

    adding MUI and redux store into app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+import store from './store/index'
+
+import { theme } from './assets/theme'
+
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+  <Provider store={store}>
+    <StrictMode>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <CssBaseline />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </StrictMode>
+  </Provider>
 )
