68de807c3cfc3ea89b1c12f6fdf1745a5c8992a5
fathan nasrullah <fathannasrullah0@gmail.com>
2023-09-30 14:16:23 +0700

    create appbar component

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+import { Button, Grid, Toolbar, } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+
+import Navigation from '../Navigation/Navigation'
+
+import { StyledAppBar, StyledIconButton } from './styles'
+
+const drawerWidth = 240
+
+function AppBar() {
+  const [mobileOpen, setMobileOpen] = useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  return (
+    <>
+      <StyledAppBar>
+        <Toolbar>
+          <StyledIconButton
+            aria-label='open navigation'
+            edge='start'
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon />
+          </StyledIconButton>
+          
+          <Grid container justifyContent='center'><Button>SKUY GASKEUN</Button></Grid>
+        </Toolbar>
+      </StyledAppBar>
+      <Navigation
+        drawerWidth={drawerWidth} 
+        handleDrawerToggle={handleDrawerToggle}
+        mobileOpen={mobileOpen}
+      />
+    </>
+  )
+}
+
+export default AppBar
