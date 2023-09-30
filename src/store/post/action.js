40bb8a3bd8328c8a790ea0d5c9ff030a092a0d50
fathan nasrullah <fathannasrullah0@gmail.com>
2023-09-30 08:55:14 +0700

    create action for post

---
+++ src/store/post/action.js
@@ -0,0 +1,67 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+
+import { creatorAddAction, creatorDeleteAction, creatorListOrDetailAction, creatorUpdateAction } from '../../utils/helpers/creator-action-helper'
+import { STATE_NAME } from '../../utils/constant'
+
+import { addPostService, deletePostService, getPostListService, updatePostService } from '../../services/post'
+
+export const getPostList = createAsyncThunk(
+  'post/list',
+  async (listParams, thunkAPI) => {
+    return (
+      await creatorListOrDetailAction(
+        thunkAPI,
+        listParams,
+        getPostListService,
+        STATE_NAME.POST_LIST,
+      )
+    )
+  }
+)
+
+export const addPost = createAsyncThunk(
+  'post/add',
+  async (addBody, thunkAPI) => {
+    return (
+      await creatorAddAction(
+        thunkAPI,
+        addBody,
+        addPostService,
+        'success.add.post',
+        'error.add.post',
+      )
+    )
+  }
+)
+
+export const updatePost = createAsyncThunk(
+  'post/update',
+  async( updateBody, thunkAPI ) => {
+    return (
+      await creatorUpdateAction(
+        thunkAPI,
+        updateBody,
+        updatePostService,
+        'success.update.post',
+        'error.update.post',
+        {}, {},
+      )
+    )
+  }
+)
+
+export const deletePost = createAsyncThunk(
+  'post/delete',
+  async( deleteParams, thunkAPI ) => {
+    return (
+      await creatorDeleteAction(
+        thunkAPI,
+        deleteParams,
+        deletePostService,
+        'success.delete.post',
+        'error.delete.post',
+        {}, {},
+      )
+    )
+  }
+)
