b3e681efea7813aef56e41690c4993f8c9cfbbba
fathan nasrullah <fathannasrullah0@gmail.com>
2023-10-02 00:44:06 +0700

    create action array for user and post

---
+++ src/utils/actions/post-actions.js
@@ -0,0 +1,12 @@
+import { Delete, Edit } from "@mui/icons-material";
+
+export const postAction = [
+  {
+    label: 'Edit',
+    Icon: Delete
+  },
+  {
+    label: 'Delete',
+    Icon: Edit
+  }
+]
---
+++ src/utils/actions/user-actions.js
@@ -0,0 +1,12 @@
+import { Delete, Edit } from "@mui/icons-material";
+
+export const userAction = [
+  {
+    label: 'Edit',
+    Icon: Delete
+  },
+  {
+    label: 'Delete',
+    Icon: Edit
+  }
+]
