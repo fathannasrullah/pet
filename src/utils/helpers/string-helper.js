489cec7e19d1710cec18b81e6087e68a3bdd3061
fathan nasrullah <fathannasrullah0@gmail.com>
2023-10-02 00:43:07 +0700

    create string helpr

---
+++ src/utils/helpers/string-helper.js
@@ -0,0 +1,8 @@
+export const limitExcededStr = (str, limit = 100, subtition = '..') => {
+  if (typeof str !== 'string') return str
+  
+  if (typeof str !== 'string' && !str.length) return '-'
+  if (str.length <= limit) return str
+
+  return `${str.slice(0, limit)}${subtition}`
+}
