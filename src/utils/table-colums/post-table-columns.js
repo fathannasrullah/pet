912e82b5add5bbf73f3ec23dc0ebcc91b09a4406
fathan nasrullah <fathannasrullah0@gmail.com>
2023-10-02 00:43:46 +0700

    create column for user and post

---
+++ src/utils/table-colums/post-table-columns.js
@@ -0,0 +1,28 @@
+export const postTableColumns = [
+  {
+    name: 'text',
+    label: 'Text',
+    minWidth: 160
+  },
+  {
+    name: 'tags',
+    label: 'Tags',
+    minWidth: 120
+  },
+  {
+    name: 'image',
+    label: 'Image',
+    minWidth: 120
+  },
+  {
+    name: 'owner',
+    label: 'User',
+    minWidth: 160
+  },
+  {
+    name: 'action',
+    label: 'Action',
+    minWidth: 190,
+    align: 'center'
+  }
+]
---
+++ src/utils/table-colums/user-table-columns.js
@@ -0,0 +1,17 @@
+export const userTableColumns = [
+  {
+    name: 'firstName',
+    label: 'Name',
+    minWidth: 160
+  },
+  {
+    name: 'picture',
+    label: 'Image',
+    minWidth: 120
+  },
+  {
+    name: 'action',
+    label: 'Action',
+    minWidth: 100 
+  }
+]
