09983ca78cc89d3c5930c00c3cc9e73f53fc7068
fathan nasrullah <fathannasrullah0@gmail.com>
2023-10-05 13:08:11 +0700

    remote white space

--- src/store/user/action.js
+++ src/store/user/action.js
@@ -64,7 +64,7 @@ export const addUser = createAsyncThunk(
 
 export const updateUser = createAsyncThunk(
   'user/update',
-  async( updateBody, thunkAPI ) => {
+  async (updateBody, thunkAPI) => {
     return (
       await creatorUpdateAction(
         thunkAPI,
@@ -79,7 +79,7 @@ export const updateUser = createAsyncThunk(
 
 export const deleteUser = createAsyncThunk(
   'user/delete',
-  async( deleteParams, thunkAPI ) => {
+  async (deleteParams, thunkAPI) => {
     return (
       await creatorDeleteAction(
         thunkAPI,
