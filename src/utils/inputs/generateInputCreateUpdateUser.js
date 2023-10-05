c01acc07d04bf04f605a905434ba9583d3b30477
fathan nasrullah <fathannasrullah0@gmail.com>
2023-10-05 11:34:00 +0700

    fix select value

--- src/utils/inputs/generateInputCreateUpdateUser.js
+++ src/utils/inputs/generateInputCreateUpdateUser.js
@@ -9,7 +9,7 @@ export const generateInputCreateUpdateUser = (actionType, userDetails) => {
       const { name } = input
       const { title, firstName, lastName, email, picture } = userDetails
 
-      if (name === 'title') return { ...input, value: title }
+      if (name === 'title') return { ...input, defaultValue: title }
       if (name === 'firstName') return { ...input, value: firstName }
       if (name === 'lastName') return { ...input, value: lastName }
       if (name === 'email') return { ...input, value: email }
