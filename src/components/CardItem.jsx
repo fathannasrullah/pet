9dd19403705fe42cdee80ba7c49fa9b813f722e9
fathan nasrullah <fathannasrullah0@gmail.com>
2023-10-10 14:02:53 +0700

    split component

---
+++ src/components/CardItem.jsx
@@ -0,0 +1,79 @@
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, Chip, Skeleton, Stack, Typography, styled } from "@mui/material"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import Likes from "./Likes"
+
+const StyledCardHeader = styled(CardHeader)(({ theme }) => ({
+  '& p': {
+    fontSize: '16px',
+    color: theme.palette.common.purple.light[1]
+  },
+  '& span': {
+    fontSize: '11px',
+    color: theme.palette.common.purple.light[3]
+  }
+}))
+
+function CardItem({
+  listLoading,
+  id,
+  avatar,
+  title,
+  subHeader,
+  image,
+  contentText,
+  tags,
+  likes
+}) {
+  return (
+    <Card sx={{ minHeight: 400, maxWidth: 345 }}>
+      <StyledCardHeader
+        avatar={<Avatar aria-label="recipe" src={avatar || <AccountCircleIcon />} />}
+        title={listLoading ? (<Skeleton sx={{ marginBottom: '5px' }} variant='rectangular' />) : (<p>{title}</p>)}
+        subheader={listLoading ? (<Skeleton variant='rectangular' />) : (<span>{subHeader}</span>)}
+      />
+      {listLoading ? (
+        <Skeleton sx={{ minHeight: '200px' }} variant='rectangular' />
+      ) : (
+        <CardMedia
+          component='img'
+          height='194'
+          image={image || 'https://i.ibb.co/V2nrLXx/no-image.jpg'}
+          alt={title}
+        />
+      )}
+      <CardContent>
+        {listLoading ? (<Skeleton variant='rectangular'/>) : (<Typography>{contentText}</Typography>)}
+      </CardContent>
+      {listLoading ? (
+        <Skeleton variant='rectangular' />
+      ) : (
+        <Box
+          sx={{
+            overflow: 'auto',
+            '::-webkit-scrollbar': {
+              display: 'none'
+            },
+            msOverflowStyle: 'none',
+            scrollbarWidth: 'none'
+          }}
+        >
+          <Stack direction='row' spacing={0.5} padding='0 10px'>
+            {tags.map((tag, index) => (<Chip key={index} label={tag} size='small' />))}
+          </Stack>
+        </Box>
+      )}
+      {listLoading ? (
+        <Skeleton sx={{ marginTop: '5px' }} variant='rectangular' />
+      ) : (
+        <CardActions disableSpacing>
+          <Likes
+            id={id}
+            likes={likes}
+          />
+        </CardActions>
+      )}
+    </Card>
+  )
+}
+
+export default CardItem
---
+++ src/components/ListGridView.jsx
@@ -0,0 +1,202 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { createSearchParams, useLocation, useNavigate } from 'react-router-dom'
+import { isEmpty } from 'lodash'
+import { Button, Grid, Pagination, PaginationItem, Stack, styled, Typography } from '@mui/material'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+import { getReadableDate } from '../utils/helpers/format-helper'
+import { limitExcededStr } from '../utils/helpers/string-helper'
+import Search from '../components/Search/Search'
+import CardItem from './CardItem'
+
+const StyledPagination = styled(Pagination)(() => ({
+  '& ul': {
+    display: 'flex',
+    flexWrap: 'nowrap',
+  }
+}))
+
+const PrevIcon = () => {
+  return (
+    <Button startIcon={<ArrowBackIosIcon />}>prev</Button>
+  )
+}
+
+const NextIcon = () => {
+  return (
+    <Button endIcon={<ArrowForwardIosIcon />}>next</Button>
+  )
+}
+
+const useNavigateParams = () => {
+  const navigate = useNavigate()
+
+  return (pathname, params) => {
+    navigate(`${pathname}?${createSearchParams(params)}`)
+  }
+}
+
+function ListGridView({
+  responseKeyName,
+  storeName,
+  listStateName,
+  listLoadingStatus,
+  onFetchList,
+  onFetchSearch
+}) {
+  const dispatch = useDispatch()
+  const { 
+    requestStatus,
+    [listStateName]: listState
+  } = useSelector((state) => state[storeName])
+
+  const listLoading = requestStatus === listLoadingStatus
+  const { [responseKeyName]: list = [], total } = listState
+  const pageAmount = Math.floor(total / 20)
+
+  const navigate = useNavigate()
+  const navigateWithParams = useNavigateParams()
+  const location = useLocation()
+  const query = new URLSearchParams(location.search)
+
+  const [fetchType, setFetchType] = useState({ name: 'initial' })
+  const [page, setPage] = useState(parseInt(query.get('page') || '1'))
+  const [searchValue, setSearchValue] = useState('')
+  const [searchTimeout, setSearchTimeout] = useState(null)
+ 
+  const handleFetchDataType = (type) => {
+    setFetchType({
+      ...fetchType,
+      name: type
+    })
+  }
+
+  const handleSearchChange = (event, emptyValue) => {
+    clearTimeout(searchTimeout)
+    
+    setSearchValue(
+      typeof emptyValue === 'string'
+        ? emptyValue
+        : event.target.value
+    )
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        setPage(1)
+        navigate('/')
+
+        handleFetchDataType('search')
+      }, 500)
+    )
+  }
+
+  const handlePageChange = (event, value) => {
+    event.preventDefault()
+    setPage(value)
+    handleFetchDataType('page-change')
+
+    navigateWithParams('/home', { page: value })
+  }
+
+  const handleFetchList = useCallback(() => {
+    let param = {
+      page: 1,
+      limit: 20
+    }
+
+    if (fetchType.name === 'search' && !isEmpty(searchValue)) dispatch(onFetchSearch(searchValue))
+
+    if ((fetchType.name === 'page-change' && page > 0) ||
+      (fetchType.name === 'initial' && page > 0) ||
+      (fetchType.name === 'search' && !isEmpty(list))) {
+      dispatch(isEmpty(searchValue)
+        ? onFetchList({
+          ...param,
+          page: page
+          })
+        : onFetchSearch({
+          ...param,
+          inputValue: searchValue,
+          page: page
+          })
+      )
+    }
+
+    if ((fetchType.name === 'initial' && page == 1) || (fetchType.name === 'search' && isEmpty(searchValue)) ) dispatch(onFetchList({ ...param }))
+  }, [fetchType, page, searchValue])
+
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'instant'
+    })
+    
+    handleFetchList()
+  }, [handleFetchList])
+
+  return (
+    <>
+      <Grid spacing={1} container>
+        <Grid item xs={12}>
+          <Search
+            searchValue={searchValue}
+            onSearchChange={handleSearchChange}
+          />
+        </Grid>
+        <Grid minHeight='80vh' item xs={12} spacing={2} container>
+          {!listLoading && isEmpty(list) && <Grid mt={20} justifyContent='center' container><Typography>NO DATA</Typography></Grid>}
+          {list.map(({
+            id,
+            owner,
+            text,
+            image,
+            publishDate,
+            tags,
+            likes
+          }) => {
+            const { firstName, lastName, picture } = owner
+            const fullName = limitExcededStr(`${firstName} ${lastName}`, 8)
+            const date = getReadableDate(publishDate, 'en')
+            const contentText = limitExcededStr(text, 33)
+
+            return (
+              <Grid item xs={6} md={3} key={id}>
+                <CardItem
+                  listLoading={listLoading}
+                  id={id}
+                  title={fullName}
+                  avatar={picture}
+                  subHeader={date}
+                  image={image}
+                  contentText={contentText}
+                  tags={tags}
+                  likes={likes}
+                />
+              </Grid>
+            )
+          })}
+        </Grid>
+        <Grid item xs={12} justifyContent='center' container>
+          <Stack overflow='auto' margin='10px 0'>
+            <StyledPagination
+              page={page}
+              count={pageAmount || 0}
+              color='primary'
+              size='large'
+              onChange={handlePageChange}
+              renderItem={(item) => (
+                <PaginationItem {...item}
+                  slots={{ previous: PrevIcon, next: NextIcon }}
+                />
+              )}
+            />
+          </Stack>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default ListGridView
